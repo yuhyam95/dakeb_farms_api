beb685c50abf5821f8e2421227dae15cb12ae81b
Yusuf Habu <yuhyam95@gmail.com>
2023-06-21 12:38:13 +0100

    Added PaySlip model

---
+++ models/PaySlip.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const paySlipSchema = new mongoose.Schema({
+   salary: {
+     type: String
+   },
+   bonus: {
+    type: String
+    },
+   deductions: {
+        type: String
+    },
+   reasonfordeductions: {
+        type: String
+    },
+   generatedby: {
+        type: String
+    },
+   payperiod: {
+        type: String
+    }, 
+   user:[{
+        name:{
+          type: String
+        },
+        email: {
+            type: String
+        },
+        department:{
+          type: String
+        },
+        position:{
+            type: String
+          }
+       }]        
+  },
+  {timestamps: true}
+  );
+
+module.exports = mongoose.model("PaySlip", paySlipSchema);
